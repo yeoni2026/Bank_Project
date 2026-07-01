697eb104b874ad5273d87ceeaded403c08e90276
yeoni2026 <yeoni2026@gmail.com>
2026-07-01 20:03:43 +0900

    테스트 함수에 pin 인자추가

--- test_account.py
+++ test_account.py
@@ -1,12 +1,12 @@
 from account import Account
 
 def test_deposit():
-    acc = Account("0001", "철수")
+    acc = Account("0001", "fff", "철수")
     acc.deposit(10000)
     assert acc.remains == 10000
 
 def test_withdraw():
-    acc = Account("0001", "영희")
+    acc = Account("0001", "ggg", "영희")
     try:
         acc.withdraw(-10000)
         assert False
